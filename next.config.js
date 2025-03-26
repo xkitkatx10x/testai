bad118e1041ed4825f94f138ccf3f4ee20229983
xkitkatx10x <131447894+xkitkatx10x@users.noreply.github.com>
2025-03-26 22:39:25 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configura Next.js per utilizzare la directory src
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
