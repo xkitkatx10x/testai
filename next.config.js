8ca383dab1fe5e2b860f3d3febc2225e9b181a7e
xkitkatx10x <131447894+xkitkatx10x@users.noreply.github.com>
2025-03-26 22:41:49 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   // Configura Next.js per utilizzare la directory src
   reactStrictMode: true,
   swcMinify: true,
+  // Specifica il percorso della directory pages
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  // Indica a Next.js che le pagine si trovano nella directory src
+  distDir: 'dist',
+  experimental: {
+    appDir: false
+  }
 }
 
 module.exports = nextConfig
